cd7044a48d4217957bb5ecb45878c028555d75ca
Galante3005 <ferdigal2007@gmail.com>
2026-09-20 14:04:05 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.Locale;
 
+ 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("pt", "BR"));
+        Locale.setDefault(new Locale("pt", "BR")); /*Garante que, não haja erros de escrrrita com letras e assento,
+                                                    comum principalmente nas saídas*/
 
         CentralAtendimento central = new CentralAtendimento();
 
@@ -30,5 +32,7 @@ public class Main {
 
         System.out.println(cadastradas + " solicitação(ões) de teste cadastrada(s) com sucesso!");
         System.out.println(central.consultarFilaAguardando());
+        Menu menu = new Menu(central);
+        menu.executar();
     }
 }
