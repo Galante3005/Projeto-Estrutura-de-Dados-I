936dd4d5494162dd3df3d222971955355705e716
Galante3005 <ferdigal2007@gmail.com>
2026-09-20 15:16:08 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Locale;
 
- 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR")); /*Garante que, não haja erros de escrrrita com letras e assento,
                                                     comum principalmente nas saídas*/
+
+        CentralAtendimento central = new CentralAtendimento();
+
         Menu menu = new Menu(central);
         menu.executar();
     }
