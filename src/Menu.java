2d7fb70b2566b4980699331f7e7fa0c6165d93fd
Galante3005 <ferdigal2007@gmail.com>
2026-09-20 14:06:58 -0300

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -166,7 +166,29 @@ public class Menu {
     }
 
     private void gerarSolicitacoesAutomaticas() {
-        // TODO: conectar aqui a funcionalidade desenvolvida pelo outro integrante.
+         String[] nomes = {
+            "Valter Augusto", "Gustavo Domingues", "Thiago Nogueira", "Camila Duarte", "Lucas Pereira",
+            "Fernanda Ramos", "Rodrigo Teixeira", "Juliana Brito", "Marcos Vinícius", "Patrícia Gomes"
+        };
+        String[] descricoes = {
+            "Sinal de internet caindo toda hora", "Fatura com valor divergente", "Aplicativo travando ao abrir",
+            "Solicitação de segunda via de boleto", "Equipamento com defeito de fábrica",
+            "Cobrança de serviço não contratado", "Lentidão no atendimento pelo chat", "Erro no cadastro do cliente",
+            "Pedido de cancelamento de plano", "Falha na instalação do serviço"
+        };
+        String[] categorias = {
+            "Suporte Técnico", "Financeiro", "Suporte Técnico", "Financeiro", "Suporte Técnico",
+            "Financeiro", "Comercial", "Suporte Técnico", "Comercial", "Suporte Técnico"
+        };
+        int[] prioridades = { 4, 2, 5, 1, 3, 2, 1, 5, 3, 4 };
+
+        int cadastradas = 0;
+        for (int i = 0; i < nomes.length; i++) {
+            central.cadastrarSolicitacao(nomes[i], descricoes[i], categorias[i], prioridades[i]);
+            cadastradas++;
+        }
+
+        System.out.println(cadastradas + " solicitação(ões) de teste cadastrada(s) com sucesso!");
     }
 
     private String lerTextoObrigatorio(String mensagem) {
